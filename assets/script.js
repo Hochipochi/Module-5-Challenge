53680f179de1fbbe8b42020db604d6c394caa9e8
Michael Smitte <smittemichael@yahoo.com>
2022-10-17 20:50:41 -0400

    worked on assignment in class

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,15 @@
-//global hour variable used for local storage
-var hour = moment().hour()
-
 $(document).ready(function () {
     //listen for save button clicks
     $('.saveBtn').on('click', function (){
         // get nearby values
-        var timeBlockEl = $('.time-block');
-        var text = timeBlockEl.children('textarea').eq(0)
+        var value = $(this).siblings('.description').val();
+        var time = $(this).parent().attr('id')
+        console.log('this', time)
+        console.log('value', value)
+
 
         // save in localStorage 
-        localStorage.setItem(hour, text);
+        localStorage.setItem(time, value)
 
         // show notification that item was saved to localStorage by adding class 'show'
         
@@ -31,6 +31,7 @@ $(document).ready(function () {
             //Conditional if/else statements to determine if currentHour is less than, equal to, or greater than currentRow.
             if (currentRow < currentHour) {
                 //change color of rows
+                //alternative to use $(this).remove/addClass('past/present/future') to alter colors via css
                 $(this).css('background-color', '#d3d3d3')
             } else if (currentRow === currentHour) {
                 $(this).css('background-color', '#ff6961')
@@ -51,6 +52,17 @@ $(document).ready(function () {
         var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
         $('#currentDay').text(currentTime)
     }
+
+    //load any saved data from localStorage
+    $('#9 .description').val(localStorage.getItem('9'))
+    $('#10 .description').val(localStorage.getItem('10'))
+    $('#11 .description').val(localStorage.getItem('11'))
+    $('#12 .description').val(localStorage.getItem('12'))
+    $('#13 .description').val(localStorage.getItem('13'))
+    $('#14 .description').val(localStorage.getItem('14'))
+    $('#15 .description').val(localStorage.getItem('15'))
+    $('#16 .description').val(localStorage.getItem('16'))
+    $('#17 .description').val(localStorage.getItem('17'))
 
     //setInterval live updates the time for our schedule (function, time interval)
     setInterval(liveTime, 1000)
